3a0745826e13fdd3c9dbb4e03e55690e780aade9
DaichiSaito <dchan1230@gmail.com>
2016-07-15 00:31:47 +0900

    プロコン練習(勝利)

--- aoj/20160714.scala
+++ aoj/20160714.scala
@@ -1,41 +1,47 @@
 object Main {
-  def ironuri(ar: Array[String], rm: Array[Int][Int], px: Int, py: Int, rx: Int, ry: Int): Array[Int] = {
-    val lx = px - 1
-    val ly = py
-    val rx = px + 1
-    val ry = py
-    val tx = px
-    val ty = py - 1
-    val bx = px
-    val by = py + 1
-    # ここから先色塗りしようと思ってた
+  def bfs(ar: Array[String], x: Int, y: Int, w: Int, h: Int): Array[String] = {
+    if (x < 0 || x >= w || y < 0 || y >= h)
+      return ar
+    if (ar(y)(x) != '.')
+      return ar
+    // イミュータブルやるにはわからなかった
+    var br = ar
+    br(y) = br(y).patch(x, "C", 1)
+    br = bfs(ar, x - 1, y, w, h)
+    br = bfs(br, x + 1, y, w, h)
+    br = bfs(br, x, y + 1, w, h)
+    br = bfs(br, x, y - 1, w, h)
+    return br
+  }
 
-  def slove(ar: Array[String],room: Array[Int]): Int = {
-    
-    var start = -1
+
+  def solve(ar: Array[String], room: Array[Int]): Int = {
+    var x = -1
     var y = -1
     for(i <- 0 until ar.length) {
       val tmp = ar(i).indexOf('@')
       if (tmp != -1) {
-        start = tmp
+        x = tmp
         y = i
       }
     }
-    y
+    var br = ar
+    br(y) = br(y).patch(x, ".", 1)
+    br = bfs(br, x, y, room(0), room(1))
+    var cnt = 0
+    for(res <- br)
+      cnt += res.count{_ == 'C'}
+    cnt
   }
 
   def main(args: Array[String]) = {
-    val room = Array(6, 9)
-    val tile = Array(
-      "....#.",
-      ".....#",
-      "......",
-      "......",
-      "......",
-      "......",
-      "......",
-      "#@...#",
-      ".#..#.")
-    println(slove(tile))
+    var room = io.StdIn.readLine().split(' ').map(_.toInt)
+    while (room(0) != 0) {
+      val ar = new Array[String](room(1))
+      for (i <- 0 until room(1))
+        ar(i) = io.StdIn.readLine()
+      println(solve(ar, room))
+      room = io.StdIn.readLine().split(' ').map(_.toInt)
+    }
   }
 }
